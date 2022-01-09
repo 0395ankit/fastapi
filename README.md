86df03dfe9a9034a0b944d42584f6569be55c166
ankit <ankit0395@gmail.com>
2022-01-09 17:24:46 +0530

    Crud using fastapi

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # fastapi
+This was created following - https://www.youtube.com/watch?v=TQfIUS52QHA
+
+Steps to follow-
+1. pip install pipenv - This helps in setting up the virtual env
+2. pipenv shell - This helps activating the env
+3. pipenv install fastapi - For the installation of FastAPI
+4. pipenv install uvicorn - This is the server from fastapi 
+5. Write all the code as written.
+6. uvicorn main:app --reload - Here main is the main.py file and app is the name of the app you set in the application main.py
+7. Following commands are very usefull provided internally by FastAPI
+    a: http://127.0.0.1:8000/docs - This gives the swagger definition of the apis automatically.
+    b: http://127.0.0.1:8000/redoc - This is more attractive UI and professional one.
---
+++ api/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+fakedb = []
+
+class Course(BaseModel):
+    id: int
+    name: str
+    price: float
+    is_early_bird: Optional[bool]=None
+
+
+@app.get('/')
+def fetch_data():
+    return {"greetings": "This is test of GET method"}
+
+
+@app.get('/courses')
+def fetch_all_courses():
+    return fakedb
+
+@app.get('/courses/{course_id}')
+def fetch_a_course(course_id: int):
+    course = course_id-1
+    return fakedb[course]
+
+@app.post('/courses')
+def add_course(course: Course):
+    fakedb.append(course.dict())
+    return fakedb[-1]
+
+
+@app.delete('/courses/{course_id}')
+def delete_courses(course_id: int):
+    fakedb.pop(course_id-1)
+    return {'task': "Successfull deletion"}
